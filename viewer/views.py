9d0d494d1b1c1310fcdabd71a36f0a993e5a11dd
AreZeiger <are.zeiger@gmail.com>
2022-05-21 13:13:32 +0300

    Created Templates

---
+++ viewer/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
+
+# def hello_view(request, s):
+#     return HttpResponse(f"Hello {s} world")
+
+
+def hello_view(request):
+    s = request.GET.get('s', '') #value od s  from url encoding or "" by default
+    n = request.GET.get('n', 0) #value of n from url encoding or 0 by default
+    adjective = "not cloudy"
+    
+    return render(
+        request,
+        "hello.html",
+        context={"adjectives":[s, n, adjective]}
+    )
+
+def index(request):
+    return render(request, template_name="base.html")
+
+
+
+def movies(request):
+
+    return render(
+    request, template_name='movies.html',
+    context={'movies': Movie.objects.all()}
+  )
+
+
